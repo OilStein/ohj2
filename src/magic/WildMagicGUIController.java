dc4edd4a62ed7149e2470bb3b3c2200086f22110
Brandon <nikobranart@gmail.com>
2021-01-19 18:08:08 +0200

    Made scene elements for main, roll and adding

--- src/magic/WildMagicGUIController.java
+++ src/magic/WildMagicGUIController.java
@@ -1,10 +1,19 @@
 package magic;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuItem;
+
 /**
  * @author Niko Sihvo
  * @version 18.1.2021
  *
  */
 public class WildMagicGUIController {
-      // TODO
+
+	@FXML MenuItem saveMenufx;
+	@FXML MenuItem closeMenufx;
+	@FXML MenuItem addMenufx;
+	@FXML MenuItem deleteMenufx;
+	@FXML MenuItem helpMenufx;
+	@FXML MenuItem aboutMenufx;
 }
