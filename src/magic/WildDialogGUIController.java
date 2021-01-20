df736d52cf2fc5d2b59c4c1f7ab63fc0713e12d9
Brandon <nikobranart@gmail.com>
2021-01-20 11:46:03 +0200

    Pictures & added items

--- src/magic/WildDialogGUIController.java
+++ src/magic/WildDialogGUIController.java
@@ -1,10 +1,46 @@
 package magic;
 
+import fi.jyu.mit.fxgui.ComboBoxChooser;
+import fi.jyu.mit.fxgui.RadioButtonChooser;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 /**
- * @author nikos
+ * @author Niko Sihvo
  * @version 19.1.2021
  *
  */
 public class WildDialogGUIController {
-      // TODO
+	
+	// Base information
+	@FXML TextField textName;
+	@FXML ComboBoxChooser<String> cbcSchool;
+	@FXML ComboBoxChooser<String> cbcLvl;
+	@FXML ComboBoxChooser<String> cbcCast;
+	@FXML ComboBoxChooser<String> cbcRange;
+	@FXML ComboBoxChooser<String> cbcDuration;
+	@FXML TextArea textDesc;
+	
+	// Component radiobuttons
+	@FXML RadioButtonChooser<Boolean> dotVerbal;
+	@FXML RadioButtonChooser<Boolean> dotSomatic;
+	@FXML RadioButtonChooser<Boolean> dotMaterial;
+	
+	
+	// Class radiobuttons
+	@FXML RadioButtonChooser<Boolean> dotArtificer;
+	@FXML RadioButtonChooser<Boolean> dotBard;
+	@FXML RadioButtonChooser<Boolean> dotCleric;
+	@FXML RadioButtonChooser<Boolean> dotDruid;
+	@FXML RadioButtonChooser<Boolean> dotPaladin;
+	@FXML RadioButtonChooser<Boolean> dotRanger;
+	@FXML RadioButtonChooser<Boolean> dotSorcerer;
+	@FXML RadioButtonChooser<Boolean> dotWarlock;
+	@FXML RadioButtonChooser<Boolean> dotWizard;
+	
+	// Save Button
+	@FXML Button buttonSave;
+	
 }
--- src/magic/WildMagicGUIController.java
+++ src/magic/WildMagicGUIController.java
@@ -1,7 +1,11 @@
 package magic;
 
+import fi.jyu.mit.fxgui.ComboBoxChooser;
+import fi.jyu.mit.fxgui.StringGrid;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 
 /**
  * @author Niko Sihvo
@@ -9,11 +13,37 @@ import javafx.scene.control.MenuItem;
  *
  */
 public class WildMagicGUIController {
-
+	
+	
+	
+	
+	/**
+	 * 					FXML ITEMS
+	 * ------------------------------------------------------
+	 */
+	
+	// File items
 	@FXML MenuItem saveMenufx;
 	@FXML MenuItem closeMenufx;
+	
+	// Edit items
 	@FXML MenuItem addMenufx;
 	@FXML MenuItem deleteMenufx;
+	
+	// Help items
 	@FXML MenuItem helpMenufx;
 	@FXML MenuItem aboutMenufx;
+	
+	// Main grid
+	@FXML StringGrid<String> spellGrid;
+	
+	// Searchbar and cropping option
+	@FXML TextField textSearch;
+	@FXML ComboBoxChooser<String> cbcClassSelect;
+	
+	// Buttons
+	@FXML Button buttonAdd;
+	@FXML Button buttonRoll;
+	@FXML Button buttonWild;
+	
 }
