146d974a0d49b63844e0289b911230a8bb345c3d
Brandon <nikobranart@gmail.com>
2021-01-20 13:11:05 +0200

    Minor fixes to dice roller

--- src/magic/WildDiceRollerGUIController.java
+++ src/magic/WildDiceRollerGUIController.java
@@ -1,10 +1,18 @@
 package magic;
 
+import fi.jyu.mit.fxgui.StringGrid;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 /**
  * @author nikos
  * @version 19.1.2021
  *
  */
 public class WildDiceRollerGUIController {
-      // TODO
+
+	@FXML Button buttonRoll;
+	@FXML TextField textRoll;
+	@FXML StringGrid<String> rollLog;
 }
